158d3f64b0cd1e40a1db8c0ab20c9b0f084acf5c
Alex Waibel <alexwaibel@users.noreply.github.com>
2021-09-04 07:21:08 +0000

    Fix build error

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,18 +1,20 @@
-import Card from './Card';
-import Hand from './Hand';
+import Card from "./Card";
+import Hand from "./Hand";
 
 export default class Player {
-    _hand: Hand;
+  _hand: Hand;
 
-    constructor() {
-        this._hand = new Hand();
-    }
+  constructor() {
+    this._hand = new Hand();
+  }
 
-    get hand(): Hand {
-        return this._hand;
-    }
+  get hand(): Hand {
+    return this._hand;
+  }
 
-    addToHand(card: Card): void {
-        this._hand.add(card);
+  addToHand(card?: Card): void {
+    if (card) {
+      this._hand.add(card);
     }
+  }
 }
